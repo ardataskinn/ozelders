2ed453af9beb9deb1f4a2c30f9ccf174360cbbca
Arda Taskin <ardalgg@gmail.com>
2026-03-01 18:14:37 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.BASE_PATH || ''
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  basePath: basePath,
-  assetPrefix: basePath,
-  images: { unoptimized: true },
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
